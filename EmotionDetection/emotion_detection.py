ef71421962dbc738f2252e8410e1c0a966b5a8a9
nobimj <nobimj@gmail.com>
2026-03-08 07:46:35 -0400

    Files modified

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,8 +5,21 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } } # Create a dictionary with the text to be analyzed
     header= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers 
-    formatted_response = json.loads(response.text)
-    emotions=formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-    emotions['dominant_emotion'] = dominant_emotion
-    return emotions # Return the response text from the API
+    
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions=formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
+        emotions['dominant_emotion'] = dominant_emotion
+        return emotions 
+    elif response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    
--- server.py
+++ server.py
@@ -1,12 +1,22 @@
-from flask import Flask, request, jsonify
+'''
+This is the server module for the flask emotion detection application
+'''
+
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route("/emotionDetector")  
+@app.route("/emotionDetector")
 def detect_emotion():
+    '''
+    Calls the emotion dection function from the Emotion Detection package
+    '''
     text_to_analyze = request.args.get("textToAnalyze", "")
     emotions = emotion_detector(text_to_analyze)
+    dominant = emotions['dominant_emotion']
+    if dominant is None:
+        return "Invalid text! Please try again!"
     formatted_str = f"For the given statement, the system response is " \
                 f"'anger': {emotions['anger']}, " \
                 f"'disgust': {emotions['disgust']}, " \
@@ -14,9 +24,12 @@ def detect_emotion():
                 f"'joy': {emotions['joy']}, " \
                 f"'sadness': {emotions['sadness']}. " \
                 f"The dominant emotion is {dominant}."
-    return formatted_str 
-@app.route("/") 
-def render_index_page(): 
+    return formatted_str
+@app.route("/")
+def render_index_page():
+    '''
+    Returns the home page of the application
+    '''
     return render_template('index.html')
 if __name__ == "__main__":
-    app.run(debug=True)  
+    app.run(debug=True)
